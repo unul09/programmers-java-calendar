b4d8e269be786386b17101ef6a8c89591ed3cb6e
unul09 <39664609+unul09@users.noreply.github.com>
2021-06-27 10:43:58 +0900

    두 수의 합 구하기

---
+++ src/unul09/calendar/sum.java
@@ -0,0 +1,17 @@
+package unul09.calendar;
+import java.util.Scanner;
+public class sum {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("두 수를 입력하세요. >");
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.print("두 수의 합은 " + (a+b) + "입니다.");
+
+	}
+
+}
