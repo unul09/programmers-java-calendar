f2f686dd6f47458ad8af4f8755c09db90d3ccd06
unul09 <39664609+unul09@users.noreply.github.com>
2021-06-27 11:45:34 +0900

    가상의 달력 찍기(윤년고려)

--- src/unul09/calendar/Calendar.java
+++ src/unul09/calendar/Calendar.java
@@ -2,7 +2,9 @@ package unul09.calendar;
 
 public class Calendar {
 
-	public static void Calendar28() {
+	public static void Calendar28(int year, int month) {
+		System.out.printf("     <<%d년 %d월>>", year, month);
+		System.out.println();
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -10,7 +12,20 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");		
 	}
-	public static void Calendar30() {
+	public static void Calendar29(int year, int month) {
+		System.out.printf("     <<%d년 %d월>>", year, month);
+		System.out.println();
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");	
+		System.out.println("29");	
+	}
+	public static void Calendar30(int year, int month) {
+		System.out.printf("     <<%d년 %d월>>", year, month);
+		System.out.println();
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -19,7 +34,9 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");	
 		System.out.println("29 30");	
 	}
-	public static void Calendar31() {
+	public static void Calendar31(int year, int month) {
+		System.out.printf("     <<%d년 %d월>>", year, month);
+		System.out.println();
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
--- src/unul09/calendar/sum.java
+++ src/unul09/calendar/sum.java
@@ -12,24 +12,30 @@ public class sum {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		int month;
+		int month, year;
 		
 		getcal: while(true) {
+			
+			System.out.print("연을 입력하세요. >");
+			year = s.nextInt();
 			
 			System.out.print("월을 입력하세요. >");
 			month = s.nextInt();
 			
 				switch(month) {
 				case 1, 3, 5, 7, 8, 10, 12: 
-					Calendar.Calendar31();
+					Calendar.Calendar31(year,month);
 					break;
 				
 				case 2:
-					Calendar.Calendar28();
+					if(year%4 == 0)
+						Calendar.Calendar29(year,month);
+					else
+						Calendar.Calendar28(year,month);
 					break;
 				
 				case 4, 6, 9, 11:
-					Calendar.Calendar30();
+					Calendar.Calendar30(year,month);
 					break;
 					
 				case -1:
