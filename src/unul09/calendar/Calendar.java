b79e417215d3bdc80beef0fc959998e4d1e8c903
unul09 <39664609+unul09@users.noreply.github.com>
2021-06-27 11:33:55 +0900

    가상의 달력 찍기

--- src/unul09/calendar/Calendar.java
+++ src/unul09/calendar/Calendar.java
@@ -2,15 +2,31 @@ package unul09.calendar;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+	public static void Calendar28() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("---------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		
-
+		System.out.println("22 23 24 25 26 27 28");		
+	}
+	public static void Calendar30() {
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");	
+		System.out.println("29 30");	
+	}
+	public static void Calendar31() {
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");	
+		System.out.println("29 30 31");	
 	}
 
 }
--- src/unul09/calendar/sum.java
+++ src/unul09/calendar/sum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class sum {
 	
 	public static void maxdays(int month) {
-		int [] days = {31,30,31,28,31,30,31,31,30,31,30,31};
+		int [] days = {31,28,31,30,31,30,31,31,30,31,30,31};
 		System.out.printf("%d월은 %d일까지 있습니다.", month, days[month-1]);
 		System.out.println("");
 	}
@@ -14,23 +14,40 @@ public class sum {
 		Scanner s = new Scanner(System.in);
 		int month;
 		
-		while(true) {
+		getcal: while(true) {
 			
 			System.out.print("월을 입력하세요. >");
 			month = s.nextInt();
-			if(month == -1) {
-				System.out.println("좋은날 되시우");
-				break;
+			
+				switch(month) {
+				case 1, 3, 5, 7, 8, 10, 12: 
+					Calendar.Calendar31();
+					break;
+				
+				case 2:
+					Calendar.Calendar28();
+					break;
+				
+				case 4, 6, 9, 11:
+					Calendar.Calendar30();
+					break;
+					
+				case -1:
+					break getcal;
+					
+				default:
+					System.out.println("제대로 입력해~");
+					
+					
+					
+				}
 			}
-			else if (month<1 || month>12)
-				System.out.println("제대로 입력해!");
-			else
-				maxdays(month);
-	
+			
+	s.close();
 		}
-		s.close();
+		
 			
 	
 	}
 
-}
+
