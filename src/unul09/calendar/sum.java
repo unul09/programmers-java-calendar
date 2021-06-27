f47e71a10e150cf676b2f85dce6e3358a033bb14
unul09 <39664609+unul09@users.noreply.github.com>
2021-06-27 11:16:06 +0900

    종료 조건을 이용해 반복하기(최대값출력 메소드 구현한상태)

--- src/unul09/calendar/sum.java
+++ src/unul09/calendar/sum.java
@@ -1,41 +1,36 @@
 package unul09.calendar;
 import java.util.Scanner;
 public class sum {
+	
+	public static void maxdays(int month) {
+		int [] days = {31,30,31,28,31,30,31,31,30,31,30,31};
+		System.out.printf("%d월은 %d일까지 있습니다.", month, days[month-1]);
+		System.out.println("");
+	}
+	
 
 	public static void main(String[] args) {
-		int [] days = {31,30,31,28,31,30,31,31,30,31,30,31};
 		
 		Scanner s = new Scanner(System.in);
+		int month;
 		
-		System.out.print("반복횟수를 입력하세요. >");
-		int repeat = s.nextInt();
-		int [] months = new int[repeat];
-		
-		System.out.print("월을 입력하세요. >");
-		for(int i=0; i<repeat; i++) {
-			months[i] = s.nextInt();
-			if(months[i]<1 || months[i]>12) {
-				System.out.println("제대로 입력해!");
-				i--;
-				}			
-		}
+		while(true) {
 			
-		
-		
-		
-			for(int i=0; i<months.length; i++) {
-				System.out.printf("%d월은 %d일까지 있습니다.", months[i], days[months[i]]);
-				System.out.println();
+			System.out.print("월을 입력하세요. >");
+			month = s.nextInt();
+			if(month == -1) {
+				System.out.println("좋은날 되시우");
+				break;
 			}
+			else if (month<1 || month>12)
+				System.out.println("제대로 입력해!");
+			else
+				maxdays(month);
+	
+		}
+		s.close();
 			
-			
-			s.close();
-		
-		
-		
-		
-		
-
+	
 	}
 
 }
